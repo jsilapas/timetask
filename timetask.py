3ed56b462226805f2f8a9d100fc0c60038e21c94
esila <esila@samus.local>
2013-06-05 22:12:02 -0400

    Formatting changes

--- timetask.py
+++ timetask.py
@@ -1,14 +1,18 @@
-are empty dictionary                                                                                                                              
+# Create an empty dictionary
 tasks={}                                                                                                                                               
-                                                                                                                                                       
-#get user input                                                                                                                                        
-task = raw_input("What is your first task?:")                                                                                                          
-tasktime = raw_input("How many minutes will it take?:")                                                                                                
-                                                                                                                                                       
-#add user input to dictionary and make pair:                                                                                                           
-tasks["task"] = "tasktime"                                                                                                                             
-#ask for more tasks                                                                                                                                    
-answer = raw_input("Do you have any other tasks?:")                                                                                                    
-if answer == "yes"                                                                                                                                     
-#loop to the beginning?                                                         else:                                                                       
-    pass                                                                        print tasks
+#Get user input
+task = raw_input("What is your first task?:")
+tasktime = raw_input("How many minutes will it take?:")
+
+#Add user input to dictionary and make pair:
+tasks["task"] = "tasktime"
+
+#Ask for more tasks
+answer = raw_input("Do you have any other tasks?:")
+
+if answer == "yes":
+    #loop to the beginning?
+else:
+    pass
+
+print tasks
