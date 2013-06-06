647e75e3b5841ebe24f82902b96036460e6e6019
Jessica Silapaswang <jsilapas@orion.(none)>
2013-06-05 21:37:36 -0400

    first commit

---
+++ timetask.py
@@ -0,0 +1,14 @@
+are empty dictionary                                                                                                                              
+tasks={}                                                                                                                                               
+                                                                                                                                                       
+#get user input                                                                                                                                        
+task = raw_input("What is your first task?:")                                                                                                          
+tasktime = raw_input("How many minutes will it take?:")                                                                                                
+                                                                                                                                                       
+#add user input to dictionary and make pair:                                                                                                           
+tasks["task"] = "tasktime"                                                                                                                             
+#ask for more tasks                                                                                                                                    
+answer = raw_input("Do you have any other tasks?:")                                                                                                    
+if answer == "yes"                                                                                                                                     
+#loop to the beginning?                                                         else:                                                                       
+    pass                                                                        print tasks
